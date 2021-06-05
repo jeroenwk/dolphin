3fac001494d70d69ee0385e1d6ba1d4b61a9df52
OatmealDome <julian@oatmealdome.me>
2021-06-05 17:53:49 -0400

    UpdateEntitlements: Correct path of entitlements plist

--- Source/iOS/DolphiniOS/Common/Build Resources/Scripts/UpdateEntitlements.sh
+++ Source/iOS/DolphiniOS/Common/Build Resources/Scripts/UpdateEntitlements.sh
@@ -12,5 +12,5 @@ fi
 # dynamic-codesigning is needed
 if [ $BUILD_FOR_JAILBROKEN_DEVICE == "YES" ]
 then
-  /usr/libexec/PlistBuddy -x -c "Merge $PROJECT_DIR/DolphiniOS/BuildResources/Entitlements_JB.plist" "$XCENT_FILE"
+  /usr/libexec/PlistBuddy -x -c "Merge \"$PROJECT_DIR/Common/Build Resources/Entitlements_JB.plist\"" "$XCENT_FILE"
 fi
