e797b58b5f6c57f9f214a1036f7fd3911effea24
OatmealDome <julian@oatmealdome.me>
2021-06-05 13:53:07 -0400

    CopyDolphinImages: Correct file names for images

--- Source/iOS/DolphiniOS/Common/Build Resources/Scripts/CopyDolphinImages.sh
+++ Source/iOS/DolphiniOS/Common/Build Resources/Scripts/CopyDolphinImages.sh
@@ -18,9 +18,9 @@ do
   
   mkdir "$ROOT_SRC_DIR/DolphinImages.xcassets/$FILENAME.imageset"
   
-  cp "$ONEX_DIR/$FILENAME.png" "$ROOT_SRC_DIR/DolphinImages.xcassets/$FILENAME.imageset/$FILENAME\@1x.png"
-  cp "$TWOX_DIR/$FILENAME.png" "$ROOT_SRC_DIR/DolphinImages.xcassets/$FILENAME.imageset/$FILENAME\@2x.png"
-  cp "$THREEX_DIR/$FILENAME.png" "$ROOT_SRC_DIR/DolphinImages.xcassets/$FILENAME.imageset/$FILENAME\@3x.png"
+  cp "$ONEX_DIR/$FILENAME.png" "$ROOT_SRC_DIR/DolphinImages.xcassets/$FILENAME.imageset/$FILENAME@1x.png"
+  cp "$TWOX_DIR/$FILENAME.png" "$ROOT_SRC_DIR/DolphinImages.xcassets/$FILENAME.imageset/$FILENAME@2x.png"
+  cp "$THREEX_DIR/$FILENAME.png" "$ROOT_SRC_DIR/DolphinImages.xcassets/$FILENAME.imageset/$FILENAME@3x.png"
   
   sed "s/FILENAME/$FILENAME/g" "$ROOT_SRC_DIR/../Common/Build Resources/DolphinImageTemplate.json.in" > "$ROOT_SRC_DIR/DolphinImages.xcassets/$FILENAME.imageset/Contents.json"
 done
