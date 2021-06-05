dbda3989d842abf136c40f33eafc3e30c3b87cb3
OatmealDome <julian@oatmealdome.me>
2021-06-05 13:55:05 -0400

    CopyDolphinImages: Change path of DolphinImages to new Common path

--- Source/iOS/DolphiniOS/Common/Build Resources/Scripts/CopyDolphinImages.sh
+++ Source/iOS/DolphiniOS/Common/Build Resources/Scripts/CopyDolphinImages.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ROOT_SRC_DIR="$PROJECT_DIR/DolphiniOS"
+ROOT_SRC_DIR="$PROJECT_DIR/Common"
 ROOT_DOLPHIN_DIR="$PROJECT_DIR/../../.."
 ANDROID_RES_DIR="$ROOT_DOLPHIN_DIR/Source/Android/app/src/main/res"
 ONEX_DIR="$ANDROID_RES_DIR/drawable-hdpi"
@@ -22,5 +22,5 @@ do
   cp "$TWOX_DIR/$FILENAME.png" "$ROOT_SRC_DIR/DolphinImages.xcassets/$FILENAME.imageset/$FILENAME@2x.png"
   cp "$THREEX_DIR/$FILENAME.png" "$ROOT_SRC_DIR/DolphinImages.xcassets/$FILENAME.imageset/$FILENAME@3x.png"
   
-  sed "s/FILENAME/$FILENAME/g" "$ROOT_SRC_DIR/../Common/Build Resources/DolphinImageTemplate.json.in" > "$ROOT_SRC_DIR/DolphinImages.xcassets/$FILENAME.imageset/Contents.json"
+  sed "s/FILENAME/$FILENAME/g" "$ROOT_SRC_DIR/Build Resources/DolphinImageTemplate.json.in" > "$ROOT_SRC_DIR/DolphinImages.xcassets/$FILENAME.imageset/Contents.json"
 done
