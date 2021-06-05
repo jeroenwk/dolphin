2a78857dcfc2e5d70808957c7b5e7d44a53d52ff
OatmealDome <julian@oatmealdome.me>
2021-06-05 03:49:20 -0400

    BuildIpa: Encapsulate paths in quotes

--- Source/iOS/DolphiniOS/Common/Build Resources/Scripts/BuildIpa.sh
+++ Source/iOS/DolphiniOS/Common/Build Resources/Scripts/BuildIpa.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-ROOT_SRC_DIR=$PROJECT_DIR/../
+ROOT_SRC_DIR="$PROJECT_DIR/../"
 EXPORT_UUID=`uuidgen`
 EXPORT_PATH="/tmp/DolphiniOS-NJB-$EXPORT_UUID"
 DOLPHIN_EXPORT_PATH="$EXPORT_PATH/dolphin_ipa_root/"
-APPLICATION_DESTINATION_PATH=$DOLPHIN_EXPORT_PATH/Payload/DolphiniOS.app
+APPLICATION_DESTINATION_PATH="$DOLPHIN_EXPORT_PATH/Payload/DolphiniOS.app"
 CODESIGN_ARGS='-f -s 0059C24602FCB60DA511DCAA08727C70C1FF9A99'
 ENTITLEMENTS_FILE="$ROOT_SRC_DIR/DolphiniOS/DolphiniOS/BuildResources/Entitlements_NJB.plist"
 ENTITLEMENTS_FILE_UNI="$ROOT_SRC_DIR/DolphiniOS/DolphiniOS/BuildResources/Entitlements_NJB_Universal.plist"
@@ -19,7 +19,7 @@ fi
 
 if [ $BUILD_FOR_PATREON == "YES" ]; then
   BUNDLE_ID="me.oatmealdome.DolphiniOS-njb-patreon-beta"
-  APPLICATION_DESTINATION_PATH=$DOLPHIN_EXPORT_PATH/Payload/DiOSPatreon.app
+  APPLICATION_DESTINATION_PATH="$DOLPHIN_EXPORT_PATH/Payload/DiOSPatreon.app"
 fi
 
 # As recommended by saurik, don't copy dot files
@@ -27,33 +27,33 @@ export COPYFILE_DISABLE
 export COPY_EXTENDED_ATTRIBUTES_DISABLE
 
 # Make directories
-mkdir -p $DOLPHIN_EXPORT_PATH
-mkdir $DOLPHIN_EXPORT_PATH/Payload
+mkdir -p "$DOLPHIN_EXPORT_PATH"
+mkdir "$DOLPHIN_EXPORT_PATH/Payload"
 
 if [ -z "$IS_CI" ]; then
-  exec > $EXPORT_PATH/archive.log 2>&1
+  exec > "$EXPORT_PATH/archive.log" 2>&1
 fi
 
 # Copy resources
-cp -r "$ARCHIVE_PATH/Products/Applications/DolphiniOS.app" $APPLICATION_DESTINATION_PATH
+cp -r "$ARCHIVE_PATH/Products/Applications/DolphiniOS.app" "$APPLICATION_DESTINATION_PATH"
 
 # Hack bundle ID
-plutil -replace "CFBundleIdentifier" -string "$BUNDLE_ID" $APPLICATION_DESTINATION_PATH/Info.plist
+plutil -replace "CFBundleIdentifier" -string "$BUNDLE_ID" "$APPLICATION_DESTINATION_PATH/Info.plist"
 
 # Resign the application
-codesign $CODESIGN_ARGS --entitlements $ENTITLEMENTS_FILE $APPLICATION_DESTINATION_PATH
-codesign $CODESIGN_ARGS --entitlements $ENTITLEMENTS_FILE $APPLICATION_DESTINATION_PATH/Frameworks/*
+codesign $CODESIGN_ARGS --entitlements "$ENTITLEMENTS_FILE" "$APPLICATION_DESTINATION_PATH"
+codesign $CODESIGN_ARGS --entitlements "$ENTITLEMENTS_FILE" "$APPLICATION_DESTINATION_PATH/Frameworks/"*
 
 # Remove the mobileprovision file
-rm $APPLICATION_DESTINATION_PATH/embedded.mobileprovision || true
+rm "$APPLICATION_DESTINATION_PATH/embedded.mobileprovision" || true
 
 # Create standard IPA
-cd $DOLPHIN_EXPORT_PATH
+cd "$DOLPHIN_EXPORT_PATH"
 zip -r ../DolphiniOS-NJB.ipa .
 
 # Resign for universal
-codesign $CODESIGN_ARGS --entitlements $ENTITLEMENTS_FILE_UNI $APPLICATION_DESTINATION_PATH
-codesign $CODESIGN_ARGS --entitlements $ENTITLEMENTS_FILE_UNI $APPLICATION_DESTINATION_PATH/Frameworks/*
+codesign $CODESIGN_ARGS --entitlements "$ENTITLEMENTS_FILE_UNI" "$APPLICATION_DESTINATION_PATH"
+codesign $CODESIGN_ARGS --entitlements "$ENTITLEMENTS_FILE_UNI" "$APPLICATION_DESTINATION_PATH/Frameworks/"*
 
 # Create universal IPA
 zip -r ../DolphiniOS-NJB-Universal.ipa .
@@ -61,5 +61,5 @@ zip -r ../DolphiniOS-NJB-Universal.ipa .
 if [ -n "$IS_CI" ]; then
   echo "$2=$EXPORT_PATH" >> $GITHUB_ENV
 else
-  open $EXPORT_PATH
+  open "$EXPORT_PATH"
 fi
