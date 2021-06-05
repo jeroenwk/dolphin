774688945c45e10967471e12219c83496b1ddaa0
OatmealDome <julian@oatmealdome.me>
2021-06-05 03:41:56 -0400

    BuildCsdbgd: Encapsulate paths in quotes

--- Source/iOS/DolphiniOS/Common/Build Resources/Scripts/BuildCsdbgd.sh
+++ Source/iOS/DolphiniOS/Common/Build Resources/Scripts/BuildCsdbgd.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-ROOT_DOLPHIN_DIR=$PROJECT_DIR/../../..
+ROOT_DOLPHIN_DIR="$PROJECT_DIR/../../.."
 
 if [ $BUILD_FOR_JAILBROKEN_DEVICE == "YES" ]; then
-  cd $ROOT_DOLPHIN_DIR/Source/iOS/csdbgd
+  cd "$ROOT_DOLPHIN_DIR/Source/iOS/csdbgd"
   make
 fi
