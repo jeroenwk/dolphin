c7c085cc76e824441c82d56b9dde03b4bc278dca
OatmealDome <julian@oatmealdome.me>
2021-06-06 21:27:21 -0400

    CpuEmulationEngineViewController: Update to match Qt functionality

--- Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/Config/Advanced/CpuEmulationEngineViewController.mm
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/Config/Advanced/CpuEmulationEngineViewController.mm
@@ -4,6 +4,12 @@
 
 #import "CpuEmulationEngineViewController.h"
 
+#if TARGET_OS_TV
+#import "DolphinATV-Swift.h"
+#else
+#import "DolphiniOS-Swift.h"
+#endif
+
 #import "Core/Config/MainSettings.h"
 #import "Core/ConfigManager.h"
 #import "Core/PowerPC/PowerPC.h"
@@ -19,62 +25,84 @@
   [super viewDidLoad];
 }
 
-- (void)viewWillAppear:(BOOL)animated
+- (NSInteger)numberOfSectionsInTableView:(UITableView*)tableView
 {
-  [super viewWillAppear:animated];
+  return 1;
+}
+
+- (NSInteger)tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section
+{
+  return PowerPC::AvailableCPUCores().size();
+}
+
+- (UITableViewCell*)tableView:(UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath*)indexPath
+{
+  PowerPC::CPUCore core = PowerPC::AvailableCPUCores()[indexPath.row];
   
-  UITableViewCell* cell = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:[self CpuCoreToRow:SConfig::GetInstance().cpu_core] inSection:0]];
-  [cell setAccessoryType:UITableViewCellAccessoryCheckmark];
+  CpuEmulationEngineCell* cell = [tableView dequeueReusableCellWithIdentifier:@"engine_cell"];
+  
+  NSString* engineName;
+  switch (core)
+  {
+    case PowerPC::CPUCore::Interpreter:
+      engineName = @"Interpreter (slowest)";
+      break;
+    case PowerPC::CPUCore::CachedInterpreter:
+      engineName = @"Cached Interpreter (slower)";
+      break;
+    case PowerPC::CPUCore::JIT64:
+      engineName = @"JIT Recompiler for x86-64 (recommended)";
+      break;
+    case PowerPC::CPUCore::JITARM64:
+      engineName = @"JIT Recompiler for ARM64 (recommended)";
+      break;
+    default:
+      engineName = @"Unknown";
+      break;
+  }
+  
+  [cell.engineLabel setText:DOLocalizedString(engineName)];
+  
+  if (core == SConfig::GetInstance().cpu_core)
+  {
+    [cell setAccessoryType:UITableViewCellAccessoryCheckmark];
+  }
+  
+  return cell;
 }
 
 - (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath
 {
-  if ([self CpuCoreToRow:SConfig::GetInstance().cpu_core] != indexPath.row)
+  PowerPC::CPUCore current_core = SConfig::GetInstance().cpu_core;
+  PowerPC::CPUCore new_core = PowerPC::AvailableCPUCores()[indexPath.row];
+  
+  if (current_core != new_core)
   {
-    UITableViewCell* cell = [tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:[self CpuCoreToRow:SConfig::GetInstance().cpu_core] inSection:0]];
-    [cell setAccessoryType:UITableViewCellAccessoryNone];
+    NSInteger currentIndex = -1;
+    for (NSInteger i = 0; i < PowerPC::AvailableCPUCores().size(); i++)
+    {
+      if (PowerPC::AvailableCPUCores()[i] == current_core)
+      {
+        currentIndex = i;
+      }
+    }
+    
+    if (currentIndex != -1)
+    {
+      UITableViewCell* cell = [tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:currentIndex inSection:0]];
+      [cell setAccessoryType:UITableViewCellAccessoryNone];
+    }
     
     UITableViewCell* new_cell = [tableView cellForRowAtIndexPath:indexPath];
     [new_cell setAccessoryType:UITableViewCellAccessoryCheckmark];
     
-    PowerPC::CPUCore core = [self RowToCpuCore:indexPath.row];
-    SConfig::GetInstance().cpu_core = core;
-    Config::SetBaseOrCurrent(Config::MAIN_CPU_CORE, core);
+    SConfig::GetInstance().cpu_core = new_core;
+    Config::SetBaseOrCurrent(Config::MAIN_CPU_CORE, new_core);
     
     [[NSUserDefaults standardUserDefaults] setBool:true forKey:@"did_deliberately_change_cpu_core"];
   }
   
   [tableView deselectRowAtIndexPath:indexPath animated:true];
-}
-
-- (int)CpuCoreToRow:(PowerPC::CPUCore)core
-{
-  switch (core)
-  {
-    case PowerPC::CPUCore::Interpreter:
-      return 0;
-    case PowerPC::CPUCore::CachedInterpreter:
-      return 1;
-    case PowerPC::CPUCore::JITARM64:
-      return 2;
-    default:
-      return -1;
-  }
-}
-
-- (PowerPC::CPUCore)RowToCpuCore:(NSInteger)row
-{
-  switch (row)
-  {
-    case 0:
-      return PowerPC::CPUCore::Interpreter;
-    case 1:
-      return PowerPC::CPUCore::CachedInterpreter;
-    case 2:
-      return PowerPC::CPUCore::JITARM64;
-    default:
-      return PowerPC::CPUCore::Interpreter;
-  }
 }
 
 @end
