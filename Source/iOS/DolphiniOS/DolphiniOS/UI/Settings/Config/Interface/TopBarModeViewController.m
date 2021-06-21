6710fa621a3821cf6a3390b19ab8649e63552292
OatmealDome <julian@oatmealdome.me>
2021-06-21 19:26:38 -0400

    ConfigSettings: Remove swipe down top bar mode

--- Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/Config/Interface/TopBarModeViewController.m
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/Config/Interface/TopBarModeViewController.m
@@ -4,6 +4,9 @@
 
 #import "TopBarModeViewController.h"
 
+#define ROW_TO_MODE(x) x + 1
+#define MODE_TO_ROW(x) x - 1
+
 @interface TopBarModeViewController ()
 
 @end
@@ -20,22 +23,22 @@
   [super viewWillAppear:animated];
   
   NSInteger mode = [[NSUserDefaults standardUserDefaults] integerForKey:@"top_bar_pull_down_mode"];
-  UITableViewCell* cell = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:mode inSection:0]];
+  UITableViewCell* cell = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:MODE_TO_ROW(mode) inSection:0]];
   [cell setAccessoryType:UITableViewCellAccessoryCheckmark];
 }
 
 - (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath
 {
   NSInteger mode = [[NSUserDefaults standardUserDefaults] integerForKey:@"top_bar_pull_down_mode"];
-  if (mode != indexPath.row)
+  if (MODE_TO_ROW(mode) != indexPath.row)
   {
-    UITableViewCell* cell = [tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:mode inSection:0]];
+    UITableViewCell* cell = [tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:MODE_TO_ROW(mode) inSection:0]];
     [cell setAccessoryType:UITableViewCellAccessoryNone];
     
     UITableViewCell* new_cell = [tableView cellForRowAtIndexPath:indexPath];
     [new_cell setAccessoryType:UITableViewCellAccessoryCheckmark];
     
-    [[NSUserDefaults standardUserDefaults] setInteger:indexPath.row forKey:@"top_bar_pull_down_mode"];
+    [[NSUserDefaults standardUserDefaults] setInteger:ROW_TO_MODE(indexPath.row) forKey:@"top_bar_pull_down_mode"];
   }
   
   [tableView deselectRowAtIndexPath:indexPath animated:true];
